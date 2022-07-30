bff1c4db2951e9d534c9df544b8f0b03e52e0bcd
liakhivan <liakhivan21@email.com>
2022-07-30 15:50:41 +0300

    Remove useless files. Add a logo and an icon. Create Header component.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import logoImg from '../../assets/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+function Header() {
+  return (
+    <header className="App-header">
+      <div className="container">
+        <div className="header-body">
+          <a className="logo" href="/">
+            <img src={logoImg} className="logo-image" alt="logo image" />
+            <p className="logo-name">MovieDatabase</p>
+          </a>
+          <div className="search">
+            <input
+              className="search-input"
+              placeholder="Search"
+              datatype="text"
+            />
+            <FontAwesomeIcon
+              icon={faSearch}
+              color="#000d26"
+              className="search-icon"
+            ></FontAwesomeIcon>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;

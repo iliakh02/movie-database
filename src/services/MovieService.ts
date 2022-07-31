21e8a5ced1c3b2c021dc9c8d04259d7b82f82337
liakhivan <liakhivan21@email.com>
2022-07-31 13:14:25 +0300

    Add mock data for getting some data if the client reaches the restriction: 'more than 100 requests per day.'

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { API_KEY, BASE_API_URI } from '../constants';
 import { MovieServiceBase } from './abstract/MovieServiceBase';
 import IMovie from '../Types/Movie.type';
+import Movies from '../MovieDatabase.json';
 
 export type MovieResponse = {
   Movies: IMovie[];
@@ -19,6 +20,10 @@ export class MovieService implements MovieServiceBase {
   async getTop250Movies(): Promise<MovieResponse> {
     const requestURL = `${BASE_API_URI}/Top250Movies/${API_KEY}`;
     const response = await axios.get<MovieResponse>(requestURL);
+    // debugger;
+    if (response.data.Movies === undefined) {
+      console.log(Movies);
+    }
     return response.data;
   }
 }

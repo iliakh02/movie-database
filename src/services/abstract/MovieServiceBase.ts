badd11e6c2bec4346f2714dcca6aa8aa58930943
liakhivan <liakhivan21@email.com>
2022-08-03 01:05:37 +0300

    add this methods in the interface.

--- src/services/abstract/MovieServiceBase.ts
+++ src/services/abstract/MovieServiceBase.ts
@@ -2,6 +2,7 @@ import IMovie from '../../Types/Movie.type';
 import { MovieListResponse } from '../MovieService';
 
 export interface MovieServiceBase {
-  // getTop250Movies(): Promise<MovieListResponse>;
+  getTop250Movies(): Promise<IMovie[]>;
   getMostPopularMovies(): Promise<IMovie[]>;
+  getSearchedMovies(searchString: string): Promise<IMovie[]>;
 }
